f186cb54413100e5d691557c54f8d94f48475d65
Craig Morris <craig.morris1@my.jcu.edu.au>
2019-03-05 10:15:23 +1000

    add game logic to mainActivity.java file to handle guessing number game. complete prac1

--- Prac1/app/src/main/java/au/edu/jcu/cp3406/guessinggame/MainActivity.java
+++ Prac1/app/src/main/java/au/edu/jcu/cp3406/guessinggame/MainActivity.java
@@ -3,10 +3,14 @@ package au.edu.jcu.cp3406.guessinggame;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Game game = new Game();
+    int magicNumber = game.magicNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +20,19 @@ public class MainActivity extends AppCompatActivity {
 
     public void checkGuess(View view) {
         Context context = getApplicationContext();
-        CharSequence message = "Checking";
-        Toast popUp = Toast.makeText(context, message, Toast.LENGTH_SHORT);
-        popUp.show();
+        String messageCorrect = "You have guessed correctly";
+        String messageIncorrect = "Sorry try again";
+
+        EditText text = (EditText) findViewById(R.id.textBox);
+        String textString = text.getText().toString();
+        // above two lines takes the EditText XML field string display value by ID and coverts to string
+
+        if (Integer.parseInt(textString) == magicNumber) {
+            Toast popUp = Toast.makeText(context, messageCorrect, Toast.LENGTH_SHORT);
+            popUp.show();
+        } else {
+            Toast popUp = Toast.makeText(context, messageIncorrect, Toast.LENGTH_SHORT);
+            popUp.show();
+        }
     }
 }
