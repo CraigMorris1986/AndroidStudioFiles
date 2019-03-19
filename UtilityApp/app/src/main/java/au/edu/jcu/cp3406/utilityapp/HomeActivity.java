ffb7089cd9af179e2504b6782d01c8972a1079c3
Craig Morris <craig.morris1@my.jcu.edu.au>
2019-03-19 18:49:33 +1000

    init commit for UtilityApp for CP3406 a1

---
+++ UtilityApp/app/src/main/java/au/edu/jcu/cp3406/utilityapp/HomeActivity.java
@@ -0,0 +1,29 @@
+package au.edu.jcu.cp3406.utilityapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    public void onClickTemperature() {
+
+    }
+
+    public void onClickWeight() {
+
+    }
+
+    public void onClickDistance() {
+
+    }
+
+    public void onClickSettings() {
+
+    }
+}
