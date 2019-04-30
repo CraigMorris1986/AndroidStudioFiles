21d28e3290ab0bfcee5c9850fe9467a600ecdfa6
Craig Morris <craig.morris1@my.jcu.edu.au>
2019-04-30 11:41:43 +1000

    App completed IDE shows errors resolving names of raw and drawable files but these are false errors the code compiles and runs as expected.

--- FoleyApp/app/src/main/java/au/edu/jcu/cp3406/foleyapp/ImageScreen.java
+++ FoleyApp/app/src/main/java/au/edu/jcu/cp3406/foleyapp/ImageScreen.java
@@ -1,12 +1,17 @@
 package au.edu.jcu.cp3406.foleyapp;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+
+import java.io.IOException;
 
 public class ImageScreen extends AppCompatActivity {
     ImageView image;
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,8 +19,42 @@ public class ImageScreen extends AppCompatActivity {
         setContentView(R.layout.activity_image_screen);
         image = findViewById(R.id.imageView);
         Intent intent = getIntent();
-        String pressedButtonText = intent.getStringExtra("buttonName");
+        final String pressedButtonText = intent.getStringExtra("buttonName");
         displayImage(pressedButtonText);
+
+        if (pressedButtonText.toLowerCase().equals("pistol")) {
+            mediaPlayer = MediaPlayer.create(this, R.raw.pistol);
+        } else if (pressedButtonText.toLowerCase().equals("shotgun")) {
+            mediaPlayer = MediaPlayer.create(this, R.raw.shotgun);
+        } else if (pressedButtonText.toLowerCase().equals("drive")) {
+            mediaPlayer = MediaPlayer.create(this, R.raw.cardrive);
+        } else if (pressedButtonText.toLowerCase().equals("ignition")) {
+            mediaPlayer = MediaPlayer.create(this, R.raw.carstart);
+        } else if (pressedButtonText.toLowerCase().equals("giggle")) {
+            mediaPlayer = MediaPlayer.create(this, R.raw.kidlaugh);
+        } else if (pressedButtonText.toLowerCase().equals("baby_talk")) {
+            mediaPlayer = MediaPlayer.create(this, R.raw.kidtalk);
+        } else if (pressedButtonText.toLowerCase().equals("message")) {
+            mediaPlayer = MediaPlayer.create(this, R.raw.notification);
+        } else if (pressedButtonText.toLowerCase().equals("mac")) {
+            mediaPlayer = MediaPlayer.create(this, R.raw.macstart);
+        } else if (pressedButtonText.toLowerCase().equals("pc")) {
+            mediaPlayer = MediaPlayer.create(this, R.raw.pcstart);
+        } else if (pressedButtonText.toLowerCase().equals("robot")) {
+            mediaPlayer = MediaPlayer.create(this, R.raw.pcspeak);
+        } else if (pressedButtonText.toLowerCase().equals("pig")) {
+            mediaPlayer = MediaPlayer.create(this, R.raw.pig);
+        } else if (pressedButtonText.toLowerCase().equals("whale")) {
+            mediaPlayer = MediaPlayer.create(this, R.raw.whale);
+        }
+
+
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.start();
+            }
+        });
     }
 
     private void displayImage(String buttonName) {
@@ -59,4 +98,6 @@ public class ImageScreen extends AppCompatActivity {
 
         }
     }
+
+
 }
