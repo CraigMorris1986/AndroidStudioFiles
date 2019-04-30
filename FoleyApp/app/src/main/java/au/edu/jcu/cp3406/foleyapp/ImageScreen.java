97a78d3c9b57df7d362cd449ddc7cd671fda772e
Craig Morris <craig.morris1@my.jcu.edu.au>
2019-04-30 10:07:21 +1000

    Init Commit Complete task 1

---
+++ FoleyApp/app/src/main/java/au/edu/jcu/cp3406/foleyapp/ImageScreen.java
@@ -0,0 +1,13 @@
+package au.edu.jcu.cp3406.foleyapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ImageScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_screen);
+    }
+}
